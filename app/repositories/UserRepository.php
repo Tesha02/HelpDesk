bf4e577ec182465a97e040c7becea48b8dda2154
Tesha02 <djordje.skolski37@gmail.com>
2026-02-06 22:13:49 +0100

    login

--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -1,7 +1,8 @@
 <?php
-require_once "../db.php";
+require_once "../app/db.php";
 
-class UserRepository {
+class UserRepository
+{
     private PDO $pdo;
 
     public function __construct()
@@ -10,30 +11,33 @@ class UserRepository {
         $this->pdo = $pdo;
     }
 
-    public function findByEmail(string $email) {
-        $sql="SELECT * FROM users WHERE users.email= :email LIMIT 1";
-        $stmt=$this->pdo->prepare($sql);
-        $stmt->execute(['email'=>$email]);
+    public function findByEmail(string $email)
+    {
+        $sql = "SELECT * FROM users WHERE users.email= :email LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['email' => $email]);
 
-        $user=$stmt->fetch(PDO::FETCH_ASSOC);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
         return $user ?? null;
     }
 
-    public function findById(int $id) {
-        $sql="SELECT * FROM users WHERE users.id= :id LIMIT 1";
-        $stmt=$this->pdo->prepare($sql);
-        $stmt->execute(['id'=>$id]);
+    public function findById(int $id)
+    {
+        $sql = "SELECT * FROM users WHERE users.id= :id LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
 
-        $user=$stmt->fetch(PDO::FETCH_ASSOC);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
         return $user ?? null;
     }
 
-    public function create(string $username, string $email, string $password_hash, string $role):int {
-        $sql="INSERT INTO users (username, email, password_hash, role)
+    public function create(string $username, string $email, string $password_hash, string $role): int
+    {
+        $sql = "INSERT INTO users (username, email, password_hash, role)
         VALUES(:username,:email,:pass,:role)";
-        $stmt=$this->pdo->prepare($sql);
-        $ok=$stmt->execute(['username'=>$username,'email'=>$email,'pass'=>$password_hash,'role'=>$role]);
-        if(!$ok) {
+        $stmt = $this->pdo->prepare($sql);
+        $ok = $stmt->execute(['username' => $username, 'email' => $email, 'pass' => $password_hash, 'role' => $role]);
+        if (!$ok) {
             return 0;
         }
 
--- public/index.php
+++ public/index.php
@@ -1,5 +1,49 @@
 <?php
+require_once "../app/helpers.php";
+require_once "../app/auth.php";
 
-
-
+$errors = [];
+$email = "";
+$pass = "";
+if (isPost()) {
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+    $errors = login($email, $pass);
+    if (empty($errors)) {
+        unset($_SESSION['old']);
+        redirect("dashboard.php");
+    } else {
+        $_SESSION['old'] = [
+            'email' => $email
+        ];
+    }
+}
 ?>
+<!doctype html>
+<html lang="sr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Help Desk - Login</title>
+</head>
+
+<body>
+    <?php if (!empty($errors)): ?>
+        <?php foreach ($errors as $e): ?>
+            <?php echo e($e) . "<br>" ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    <h1>LOGIN</h1>
+    <form method="POST">
+        <label>Email:</label><br>
+        <input type="email" name="email" value=<?php echo e(old('email') ?? ""); ?> /><br>
+        <label>Password:</label><br>
+        <input type="password" name="password" /><br><br>
+        <button type="submit">Uloguj se</button>
+        <?php unset($_SESSION['old']); ?>
+    </form>
+
+
+</body>
+
+</html>
